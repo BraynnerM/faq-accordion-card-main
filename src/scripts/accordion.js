a45cc9e77fe91edc2b2a2d38589d86059ae09fd3
unknown <braynnermarques@hotmail.com>
2023-06-26 20:04:31 -0300

    comitando mais alguns ajustes

--- src/scripts/accordion.js
+++ src/scripts/accordion.js
@@ -1,6 +1,8 @@
 const accordionHeaders = document.querySelectorAll('.accordion-header');
 
 accordionHeaders.forEach(header => {
+  const image = header.querySelector('img');
+
   header.addEventListener('click', () => {
     const isActive = header.classList.contains('active');
     const accordionContent = header.parentElement.querySelector('.accordion-content');
@@ -16,10 +18,12 @@ accordionHeaders.forEach(header => {
       header.classList.add('active');
       header.style.fontWeight = 'bold';
       accordionContent.style.display = 'block';
+      image.style.transform = 'scaleY(-1)';
     } else {
       header.classList.remove('active');
       header.style.fontWeight = 'normal';
       accordionContent.style.display = 'none';
+      image.style.transform = 'none';
     }
   });
 });
